a9da8a02234cf020b75b9a2c461fde05c699bb9c
piotrsoczewka <piotrsoczewka92@gmail.com>
2020-09-08 07:34:37 +0200

    sql creating user and database

--- creating_user_and_database.sql
+++ creating_user_and_database.sql
@@ -1,3 +1,4 @@
+#creating user and database for storing gathered data
 create database lotto;
 create user 'piotrsoczewka'@'localhost' identified by 'qwerty';
 grant all privileges on lotto.* to 'piotrsoczewka'@'localhost';
