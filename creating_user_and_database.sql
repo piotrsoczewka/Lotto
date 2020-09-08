48532e075f38159af8aa05f44f84572ac909c48e
piotrsoczewka <piotrsoczewka92@gmail.com>
2020-09-08 07:58:54 +0200

    data gathering - finished code

--- creating_user_and_database.sql
+++ creating_user_and_database.sql
@@ -2,3 +2,7 @@
 create database lotto;
 create user 'piotrsoczewka'@'localhost' identified by 'qwerty';
 grant all privileges on lotto.* to 'piotrsoczewka'@'localhost';
+use lotto;
+show tables;
+select * from polish_cities;
+select * from polish_provinces;
