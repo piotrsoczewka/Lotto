a6df3813b4f9d70124fee75ccfcb410548b73d92
piotrsoczewka <piotrsoczewka92@gmail.com>
2020-09-10 14:40:31 +0200

    git practice 10.09.2020

--- creating_user_and_database.sql
+++ creating_user_and_database.sql
@@ -1,3 +1,8 @@
+#creating user and database for storing gathered data
 create database lotto;
 create user 'piotrsoczewka'@'localhost' identified by 'qwerty';
 grant all privileges on lotto.* to 'piotrsoczewka'@'localhost';
+use lotto;
+show tables;
+select * from polish_cities;
+select * from polish_provinces;
