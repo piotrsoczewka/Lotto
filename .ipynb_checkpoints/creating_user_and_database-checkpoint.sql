8f355c42251d6d210d838eb088abb0356a337418
piotrsoczewka <piotrsoczewka92@gmail.com>
2021-02-15 19:40:36 +0100

    project update 15.02.2021

--- .ipynb_checkpoints/creating_user_and_database-checkpoint.sql
+++
@@ -1,4 +0,0 @@
-#creating user and database for storing gathered data
-create database lotto;
-create user 'piotrsoczewka'@'localhost' identified by 'qwerty';
-grant all privileges on lotto.* to 'piotrsoczewka'@'localhost';
